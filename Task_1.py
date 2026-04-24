d26aa1d3743a9650e38e2056c04275ee908dd645
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2026-04-24 23:09:26 +0200

    fix task_1

--- Task_1.py
+++ Task_1.py
@@ -1,25 +1,24 @@
 from datetime import datetime
+
+
 def get_days_from_today(date: str) -> int:
 
     try:
-        given_data = datetime.strptime(date, "%Y-%m-%d").date()
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
     except ValueError:
         raise ValueError(f"Некоректний формат дати: '{date}'. Очікується 'РРРР-ММ-ДД'.")
-    
+
     today = datetime.today().date()
-    delta = today - given_data
+    delta = today - given_date
     return delta.days
 
 
 
 if __name__ == "__main__":
-    examples = ["2021-10-09", "2020-01-01", "2099-12-31", "2000-06-15"]
+    examples = ["2021-10-09", "2020-01-01", "2099-12-31", "2000-06-15", "не-дата"]
+
     for d in examples:
-        print(f"get_days_from_today('{d}') = {get_days_from_today(d)}")
-
-
-
-try:
-    get_days_from_today("не-дата")
-except ValueError as e:
-    print(f"Помилка: {e}")
+        try:
+            print(f"get_days_from_today('{d}') = {get_days_from_today(d)}")
+        except ValueError as e:
+            print(f"Помилка: {e}")
